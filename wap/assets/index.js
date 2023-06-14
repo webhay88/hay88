28f2e2dd7e8cdb87175b14bdadd78662f7ecbfbc
iwin88789 <134831098+iwin88789@users.noreply.github.com>
2023-06-14 13:06:24 +0700

    hay88

---
+++ wap/assets/index.js
@@ -0,0 +1,56 @@
+// ms box show and hide
+$("document").ready(function () {
+	$(".btn1").click(function () {
+		$(".main_box_ms").show(80);
+	});
+	$(".exit").click(function () {
+		$(".main_box_ms").hide(80);
+	});
+	$(".btn1").click(function () {
+		$(".exit").show(80);
+	});
+	$(".exit").click(function () {
+		$(".exit").hide(80);
+	});
+});
+
+// ms zone
+var urlList = ['hay8811.com', 'hay8822.com', 'hay8833.com', 'hay8844.com',
+	'hay8855.com', 'hay8866.com', 'hay8877.com', 'hay8888.com', 'hay8899.com'
+];
+
+function urlli() {
+	for (i = 0; i <= 9; i++) {
+		$('.ulList li').eq(i).find('a').attr("href", 'https://www.' + urlList[i]);
+		// $('.ulList li').eq(i).find('span.url').html(urlList[i]);
+		$('.ulList li').eq(i).find('a.url').find('p').html(urlList[i]);
+	}
+}
+urlli()
+setTimeout(function () {
+	$('#lineMs0').html(Math.ceil(Math.random() * 50) + 'ms')
+}, 300)
+setTimeout(function () {
+	$('#lineMs1').html(Math.ceil(Math.random() * 60) + 'ms')
+}, 400)
+setTimeout(function () {
+	$('#lineMs2').html(Math.ceil(Math.random() * 50) + 'ms')
+}, 200)
+setTimeout(function () {
+	$('#lineMs3').html(Math.ceil(Math.random() * 100) + 'ms')
+}, 400)
+setTimeout(function () {
+	$('#lineMs4').html(Math.ceil(Math.random() * 80) + 'ms')
+}, 500)
+setTimeout(function () {
+	$('#lineMs5').html(Math.ceil(Math.random() * 70) + 'ms')
+}, 600)
+setTimeout(function () {
+	$('#lineMs6').html(Math.ceil(Math.random() * 100) + 'ms')
+}, 600)
+setTimeout(function () {
+	$('#lineMs7').html(Math.ceil(Math.random() * 50) + 'ms')
+}, 600)
+setTimeout(function () {
+	$('#lineMs8').html(Math.ceil(Math.random() * 60) + 'ms')
+}, 600)
